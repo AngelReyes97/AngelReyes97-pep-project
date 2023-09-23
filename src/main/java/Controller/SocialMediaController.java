45117c8e9c85be93c38ad447942a69adde9d4e52
Angel Reyes <angelreyes968@icloud.com>
2023-09-23 14:51:48 +0000

    not completed save progress

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -2,6 +2,8 @@ package Controller;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import Model.Account;
+import Service.AccountService;;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +11,10 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accountService;
+    public SocialMediaController(){
+        accountService = new AccountService();
+    }
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -17,6 +23,7 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::registerUser);
 
         return app;
     }
@@ -27,6 +34,29 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    private void registerUser(Context context) {
+        Account newUser = context.bodyAsClass(Account.class);
+
+        if (newUser.getUsername().isBlank() || newUser.getPassword().length() < 4) {
+            context.status(400);
+            return;
+        }
+
+        Account existingUser = accountService.getUserByUsername(newUser.getUsername());
+
+        if (existingUser == null) {
+            Account createdUser = accountService.addAccount(newUser);
+
+            // Return the newly created user with account_id in the response
+            context.json(createdUser);
+            context.status(200); // OK
+        }
+        else {
+            context.status(400); // Bad Request
+            return;
+        }
     }
 
 
---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,29 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+import java.util.List;
+
+public class AccountService {
+    AccountDAO accountDAO;
+
+    public AccountService () {
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO) {
+        this.accountDAO = accountDAO;
+    }
+
+    public Account addAccount(Account account) {
+        return accountDAO.insertAccount(account);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountDAO.getAllAccounts();
+    }
+
+    public Account getUserByUsername(String user) {
+        return accountDAO.getUserByUsername(user);
+    }
+}
